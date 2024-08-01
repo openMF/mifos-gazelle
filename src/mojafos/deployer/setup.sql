e15e1e25530169369b17a2670ec221993d300228
Tom Daly <tdaly61@gmail.com>
2024-08-01 08:51:32 +0000

    start of copy from mojafos to mifos-gazelle

--- src/mojafos/deployer/setup.sql
+++
@@ -1,6 +0,0 @@
-DROP DATABASE `tenants`;
-CREATE DATABASE `tenants`;
-GRANT ALL PRIVILEGES ON `tenants`.* TO 'mifos';
-GRANT ALL PRIVILEGES ON `rhino`.* TO 'mifos';
-GRANT ALL PRIVILEGES ON `gorilla`.* TO 'mifos';
-GRANT ALL ON *.* TO 'root'@'%';
